e7e874bd53cf6613cef701e60f391d558dc6b1a5
Andrew Koo <andrewkykoo@gmail.com>
2023-06-07 12:45:39 -0400

    Initial Commit

---
+++ Books App/Books_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Books_AppApp.swift
+//  Books App
+//
+//  Created by Andrew Koo on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Books_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
